54cfb89add8746db39e1f14d46e2971a346400fb
--global <savadiafoundation@gmail.com>
2026-03-31 16:06:15 +0530

    DD

--- Frontend/src/lib/axios.ts
+++ Frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://gauchara-8368.onrender.com/api';
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://192.168.0.249:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://gauchara-8368.onrender.com/api';
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://192.168.0.249:8000/api';
 
 // Create axios instance
 const axiosInstance = axios.create({
